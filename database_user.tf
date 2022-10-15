1b129a256fee67bcd2b7d75c01ae316651d4f837
Piti.C <276376+ninefyi@users.noreply.github.com>
2022-10-16 00:49:27 +0800

    init project

--- database_user.tf
+++ database_user.tf
@@ -2,7 +2,7 @@
 resource "mongodbatlas_database_user" "user" {
   username           = var.dbuser
   password           = var.dbuser_password
-  project_id         = mongodbatlas_project.project.id
+  project_id         = var.project.id
   auth_database_name = "admin"
 
   roles {
